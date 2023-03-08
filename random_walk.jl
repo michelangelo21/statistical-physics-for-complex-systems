73217d27b1d44c8fccc7a0f9053946fa9535acb6
Michał Łukomski <michal.lukomski21@gmail.com>
2023-03-08 12:31:04 +0100

    line fit

--- random_walk.jl
+++ random_walk.jl
@@ -1,7 +1,9 @@
 using Random
 using Plots
+using Statistics
+using LsqFit
 
-K = 10_000
+N = 10_000
 
 function step(pos)
     r = rand()
@@ -16,13 +18,12 @@ function step(pos)
     end
 end
 
-function random_walk(K)
-    K += 1
-    currpos = (0,0)
-    history = zeros(Int, 2, K)
-    history
+function random_walk_history(N)
+    N += 1
+    currpos = (0, 0)
+    history = zeros(Int, 2, N)
 
-    for i in 2:K
+    for i in 2:N
         currpos = step(currpos)
         history[1, i] = currpos[1]
         history[2, i] = currpos[2]
@@ -30,7 +31,45 @@ function random_walk(K)
     return history
 end
 
-history = random_walk(K)
+function random_walk(N)
+    N += 1
+    currpos = (0, 0)
+    # history = zeros(Int, 2, N)
+
+    for i in 2:N
+        currpos = step(currpos)
+        # history[1, i] = currpos[1]
+        # history[2, i] = currpos[2]
+    end
+    return currpos
+end
+
+history = random_walk_history(K)
 plot(history[1, :], history[2, :], label=:none)
 plot!([0], [0], seriestype=:scatter, markersize=3, markerstrokewidth=0, color=:red, label="Start")
 title!("Random Walk for K=$(K) steps")
+
+function last_distance(N)
+    x, y = random_walk(N)
+    return √(x^2 + y^2)
+end
+
+K = 10_000
+R̄(N, K) = mean(last_distance(N) for _ in 1:K)
+
+Ns = 10:10:1000
+Rs = [R̄(n, K) for n in Ns]
+
+
+# @. model(x, p) = p[1] * x
+@. model(x, p) = p[1] * x + p[2]
+p0 = [1.0, 1.0]
+fit = curve_fit(model, Ns, Rs .^ 2, p0)
+a, b = coef(fit)
+
+scatter(Ns, Rs .^ 2, label=:none, markersize=2)
+plot!(Ns, model(Ns, fit.param), label="Fit: a=$a, b=$b", linewidth=2)
+xlabel!("N")
+ylabel!("<R²>")
+
+
