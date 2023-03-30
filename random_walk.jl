2c5317ece25d95502fd9d8480d5f6c4cf390a735
Michał Łukomski <michal.lukomski21@gmail.com>
2023-03-30 11:53:50 +0200

    deleted comments

--- random_walk.jl
+++ random_walk.jl
@@ -24,7 +24,6 @@ function random_walk_history(N)
     N += 1
     currpos = (0, 0)
     history = zeros(Int, 2, N)
-
     for i in 2:N
         currpos = step(currpos)
         history[1, i] = currpos[1]
@@ -36,20 +35,11 @@ end
 function random_walk(N)
     N += 1
     currpos = (0, 0)
-    # history = zeros(Int, 2, N)
-
     for i in 2:N
         currpos = step(currpos)
-        # history[1, i] = currpos[1]
-        # history[2, i] = currpos[2]
     end
     return currpos
 end
-
-# histories = [random_walk_history(n) for n in Ns_history]
-# plot!([0], [0], seriestype=:scatter, markersize=3, markerstrokewidth=0, color=:green, label="Start (0, 0)")
-# plot!([history[1, end]], [history[2, end]], seriestype=:scatter, markersize=3, markerstrokewidth=0, color=:red, label="End")
-
 
 Ns_history = (100, 1000, 10_000)
 histories = [random_walk_history(n) for n in Ns_history]
@@ -63,11 +53,7 @@ plts = map(1:length(Ns_history)) do i
 end
 
 plot(plts..., layout=(1, length(Ns_history)), size=(1200, 400), dpi=600)
-# lim = maximum(abs, histories[end]) + 5
-# xlims!(-lim, lim)
-# ylims!(-lim, lim)
 savefig("random_walk_histories.png")
-
 
 function last_distance(N)
     x, y = random_walk(N)
@@ -79,15 +65,10 @@ R̄(N, K) = mean(last_distance(N) for _ in 1:K)
 
 Ns = vcat([100:100:900, 1000:1000:9000, 10_000:10_000:90_000, 100_000:100_000:1_000_000]...)
 @time Rs = [R̄(n, K) for n in Ns]
-# @time last_distance(100_000)
-# @time R̄(1000_000, K)
 
-
-# @. model(x, p) = p[1] * x
 @. model(x, p) = p[1] * x + p[2]
 p0 = [1.0, 1.0]
 fit = curve_fit(model, Ns, Rs .^ 2, p0)
-# a, b = coef(fit)
 a, b = coef(fit) .± stderror(fit)
 
 scatter(Ns, Rs .^ 2, label="Squared mean over K=10 000 repetitions", markersize=2, dpi=600)
@@ -96,4 +77,3 @@ xlabel!("N")
 ylabel!("<R>²")
 title!("Squared mean distance from origin after N steps")
 savefig("random_walk_squared_mean_distance.png")
-
