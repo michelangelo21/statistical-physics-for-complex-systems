fd3d601ed1b9a47a38c32f6e8373d8de50d3e4fb
Michał Łukomski <michal.lukomski21@gmail.com>
2023-04-12 13:22:32 +0200

    zajecia

--- Ehrenfest_model.jl
+++ Ehrenfest_model.jl
@@ -1,36 +1,62 @@
 using Random
 using Plots
 
-N = 100
-MC_STEPS = 200
-p = 0.1 # probability of change
-
-fleas = trues(N)
-trajectories = [copy(fleas)]
-for j in 1:MC_STEPS
-    for i in 1:N
-        if rand() < p
-            flee_idx = rand(1:N) # speedup, cause only rand if rand() < p
-            # fleas[flee_idx] *= -1
-            fleas[flee_idx] = !fleas[flee_idx]
+function ehrenfest_trajectories(n; p=0.4, mc_steps=10_000)
+    fleas = trues(n)
+    trajectories = [copy(fleas)]
+    for j in 1:mc_steps
+        for i in 1:n
+            if rand() < p
+                flee_idx = rand(1:n) # speedup, cause only rand if rand() < p
+                # fleas[flee_idx] *= -1
+                fleas[flee_idx] = !fleas[flee_idx]
+            end
         end
+        push!(trajectories, copy(fleas))
     end
-    push!(trajectories, copy(fleas))
+    return trajectories
 end
 
-fleas
-trajectories
-trajectories_M = hcat(trajectories...)
+function ehrenfest(n; p=0.4, mc_steps=10_000)
+    fleas = trues(n)
+    N_As = map(1:mc_steps) do _
+        for i in 1:n
+            if rand() < p
+                flee_idx = rand(1:n) # speedup, cause only rand if rand() < p
+                # fleas[flee_idx] *= -1
+                fleas[flee_idx] = !fleas[flee_idx]
+            end
+        end
+        return sum(fleas)
+    end
+    return [n; N_As]
+end
 
-plot(0:MC_STEPS, getindex.(trajectories, 1), label=:none)
-xlabel!("time [MCS]")
-ylabel!("dog")
-title!("flea, p=$(p)")
+N = 500
+mc_steps = 10_000
+# trajectories = ehrenfest_trajectories(10, mc_steps=mc_steps)
+# trajectories_M = hcat(trajectories...)
+N_As = ehrenfest(N, mc_steps=mc_steps)
 
-trajectories_M[1, :]
-n_fleas = sum(trajectories_M, dims=1)
-plot(0:MC_STEPS, n_fleas', label="dog_A")
-plot!(0:MC_STEPS, N .- n_fleas', label="dog_B")
+# plot(0:MC_STEPS, getindex.(trajectories, 1), label=:none)
+# xlabel!("time [MCS]")
+# ylabel!("dog")
+# title!("flea, p=$(p)")
+
+# N_As = sum(trajectories_M, dims=1)'
+
+plot(0:mc_steps, N_As, label="N_A")
+plot!(0:mc_steps, N .- N_As, label="N_B")
 xlabel!("time [MCS]")
 ylabel!("number of fleas")
 title!("N=$(N) fleas, p=$(p)")
+
+# histogram(N_As, xlims=(0, n), normalize=:pdf)
+histogram(N_As, normalize=:pdf)
+scatterhist(N_As, normalize=:pdf)
+
+analytical(n, N) = binomial(BigInt(N), BigInt(n)) / (BigInt(1) << 500)
+plot!(0:N, analytical.(0:N, N), label="analytical")
+
+map(n -> analytical(n, N), 0:N)
+
--- normal_dist.jl
+++ normal_dist.jl
@@ -1,3 +1,19 @@
 using Random
+using Plots
 
+K = 1000
+normal = randn(K)
 
+histogram(normal)
+
+u = rand(K)
+v = rand(K)
+
+σ = 10
+μ = 7
+
+z₁ = @. μ + σ * √(-2 * log(u)) * cos(2 * π * v)
+z₂ = @. μ + σ * √(-2 * log(u)) * sin(2 * π * v)
+
+histogram(z₁)
+histogram(z₂)
